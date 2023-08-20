d9eb3fe3870fdcb8743c89325a0376a57343fe0d
Alkhodr Hallal <alkhodrhallal@gmail.com>
2023-08-21 01:51:55 +0300

    Add Main.py and EmployeeDatabse.txt also can read and add to code dictionary

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# The Function below is to import the text file that contains employee data and
+# add them to a local dictionay called employee_dict.
+def read_employee_database(file_name):
+    employee_dict = {}
 
+    with open(file_name, 'r') as file:
+        for line in file:
+            employee_data = line.strip().split(', ')
+            employee_id, username, join_date, gender, salary = employee_data
+            
+            # Store employee details in the dictionary
+            employee_dict[employee_id] = {
+                'username': username,
+                'join_date': join_date,
+                'gender': gender,
+                'salary': int(salary)
+            }
+
+    return employee_dict
